8fcf1a5cee2e3e8d08eb249a0305445df92124f6
SATIVA-OG <chotikun.ka@vru.ac.th>
2026-05-02 15:20:32 +0700

    2

--- index.js
+++ index.js
@@ -1,45 +1,46 @@
 const express = require('express');
-const axios = require('axios');
+const { messagingApi, middleware } = require('@line/bot-sdk');
+
 const app = express();
 
-app.use(express.json());
+const config = {
+  channelAccessToken: 'OgyNcZkBmyTBECdvOuFL77LzApWqboI8eLffl870/eH1lj1xoQ+Or0OND0X3ZW/oJWWr+wTXbpFHWZCAXaGp9hEblnVlkyimWs8WP7nCPFbDJylIJ0bRD0Y3hcpbUY9YUWHJH3kCNrdBZIK+aTJ9igdB04t89/1O/w1cDnyilFU=',
+  channelSecret: '53d5f7a441019b28470c0fa863a875fb'
+};
 
-const TOKEN = 'OgyNcZkBmyTBECdvOuFL77LzApWqboI8eLffl870/eH1lj1xoQ+Or0OND0X3ZW/oJWWr+wTXbpFHWZCAXaGp9hEblnVlkyimWs8WP7nCPFbDJylIJ0bRD0Y3hcpbUY9YUWHJH3kCNrdBZIK+aTJ9igdB04t89/1O/w1cDnyilFU=';
 
-app.post('/webhook', async (req, res) => {
-  try {
-    console.log(JSON.stringify(req.body, null, 2));
-
-    const events = req.body.events;
-
-    for (let event of events) {
-      if (event.type === 'message') {
-
-        const response = await axios.post(
-          'https://api.line.me/v2/bot/message/reply',
-          {
-            replyToken: event.replyToken,
-            messages: [
-              { type: 'text', text: 'สวัสดีนี้โชติเองครับ' }
-            ]
-          },
-          {
-            headers: {
-              'Content-Type': 'application/json',
-              'Authorization': `Bearer ${TOKEN}`
-            }
-          }
-        );
-
-        console.log('Reply success:', response.data);
-      }
-    }
-
-    res.sendStatus(200);
-  } catch (err) {
-    console.error('ERROR:', err.response?.data || err.message);
-    res.sendStatus(500);
-  }
+const client = new messagingApi.MessagingApiClient({
+  channelAccessToken: config.channelAccessToken
 });
 
-app.listen(3000, () => console.log('Server running on port 3000'));
+
+app.post('/webhook', middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error('Error in /webhook:', err);
+      res.status(500).end();
+    });
+});
+
+
+async function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+
+
+  return client.replyMessage({
+    replyToken: event.replyToken,
+    messages: [{
+      type: 'text',
+      text: 'สวัสดีนี้โชติเองครับ'
+    }]
+  });
+}
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
